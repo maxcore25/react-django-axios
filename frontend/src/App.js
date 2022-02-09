2338708bcbf1859dcc93e7f782aee7dff236d046
maxcore25 <mkorobov2002@mail.ru>
2022-02-09 15:53:56 +0300

    API Output done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return <div className='App'></div>;
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000').then(res => {
+      setEmployees(res.data);
+    });
+  }, []);
+  console.log(employees);
+  return (
+    <div className='App'>
+      <h1>Hello World</h1>
+      {employees.map(employee => (
+        <div key={employee.employee}>
+          {employee.employee} - {employee.department}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
